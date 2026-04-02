2f896018043b7316bfd4fee44ed2cdde33fbdd97
Seif-96 <seif365.s.seif@gmail.com>
2026-04-02 23:38:04 +0200

    edit

--- src/utilities.ts
+++ src/utilities.ts
@@ -3,7 +3,9 @@ import { cookies } from 'next/headers';
 
 export async function getAccessToken() {
   const cooke = await cookies();
-  const myToken = cooke.get('next-auth.session-token')?.value;
+  const tokenDev = cooke.get('next-auth.session-token')?.value;
+  const tokenProd = cooke.get('__Secure-next-auth.session-token')?.value;
+  const myToken = tokenDev || tokenProd;
   const decodedToken = await decode({ token: myToken, secret: process.env.NEXTAUTH_SECRET! });
   const token = decodedToken?.routeToken;
   return token;
