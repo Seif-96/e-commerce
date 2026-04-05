aabf1d8a7e20d4f4081ba4645c55ebc27392312d
Seif-96 <seif365.s.seif@gmail.com>
2026-04-05 12:00:34 +0200

    edit

--- src/utilities.ts
+++ src/utilities.ts
@@ -2,28 +2,11 @@ import { decode } from 'next-auth/jwt';
 import { cookies } from 'next/headers';
 
 export async function getAccessToken() {
-  const cooke = cookies();
-
-  const tokenDev = (await cooke).get('next-auth.session-token')?.value;
-  const tokenProd = (await cooke).get('__Secure-next-auth.session-token')?.value;
-
+  const cooke = await cookies();
+  const tokenDev = cooke.get('next-auth.session-token')?.value;
+  const tokenProd = cooke.get('__Secure-next-auth.session-token')?.value;
   const myToken = tokenDev || tokenProd;
-
-  // 🔥 أهم سطر
-  if (!myToken) {
-    console.log('Login First');
-    return null;
-  }
-
-  try {
-    const decodedToken = await decode({
-      token: myToken,
-      secret: process.env.NEXTAUTH_SECRET!,
-    });
-
-    return decodedToken?.routeToken || null;
-  } catch (err) {
-    console.error('❌ Decode failed:', err);
-    return null;
-  }
+  const decodedToken = await decode({ token: myToken, secret: process.env.NEXTAUTH_SECRET! });
+  const token = decodedToken?.routeToken;
+  return token;
 }
